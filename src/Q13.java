f5285f36c2df48ae6b46551af5a6aa0d9431bc43
hayafuneno <hayafune.syota@spi-jp.com>
2024-02-27 19:00:54 +0900

    名前、年齢、身長、体重の出力するプログラム

--- src/Q13.java
+++ src/Q13.java
@@ -1,20 +1,20 @@
 
 public class Q13 {
-
+	// 名前、年齢、身長、体重の出力
 	public static void main(String[] args) {
 		String name = "タロウ";
-		int age =20;
-		double height =170.0;
-		double weight =60.5;
+		int age = 20;
+		double height = 170.0;
+		double weight = 60.5;
 //		boolean healty = true;
-		
-		System.out.println("私は"+name+ "です"+ age+ "歳です");
-		
-		String result1 =("私は"+name+"です。私は"+age+"歳です");
-	 	String result2 =("身長は"+height+"cmです。体重は"+weight+"kgです");
-	
-	 	System.out.println(result1);	
-	 	System.out.println(result2);
+
+		System.out.println("私は" + name + "です" + age + "歳です");
+
+		String result1 = ("私は" + name + "です。私は" + age + "歳です");
+		String result2 = ("身長は" + height + "cmです。体重は" + weight + "kgです");
+
+		System.out.println(result1);
+		System.out.println(result2);
 	}
 
 }
