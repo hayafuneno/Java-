7fa0b216965c9d2cf7e2f391871ca7946a10e690
hayafuneno <hayafune.syota@spi-jp.com>
2024-02-27 19:24:42 +0900

    「D047:メダリストの表示」金・銀・銅メダリストの国名・選手名が順番に与えられ、それぞれの国名・選手名の前に獲得メダル ("Gold","Silver", "Bronze") を出力するプログラム

--- src/Q4.java
+++ src/Q4.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 //https://paiza.jp/career/challenges/140
 public class Q4 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);//java.util.Scannerをインポートする
-		
+		Scanner sc = new Scanner(System.in);// java.util.Scannerをインポートする
+
 		String gold = sc.nextLine();// キーボードで入力された値を取得する
 		String silver = sc.nextLine();// キーボードで入力された値を取得する
 		String bronze = sc.nextLine();// キーボードで入力された値を取得する
-		
-		System.out.println("Gold "+gold);//上記9行で入力したモノを出力
-		System.out.println("Silver "+ silver);//上記10行で入力したモノを出力
-		System.out.println("Bronze "+ bronze);//上記11行で入力したモノを出力
+
+		System.out.println("Gold " + gold);// 上記9行で入力したモノを出力
+		System.out.println("Silver " + silver);// 上記10行で入力したモノを出力
+		System.out.println("Bronze " + bronze);// 上記11行で入力したモノを出力
 	}
 }
