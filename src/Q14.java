775b5155dbc6ae4669ff9981f811069080f5e09b
hayafuneno <hayafune.syota@spi-jp.com>
2024-02-27 18:58:53 +0900

    「数値が0以上かどうか」を画面に表示するプログラム

--- src/Q14.java
+++ src/Q14.java
@@ -4,7 +4,7 @@ public class Q14 {
 	public static void main(String[] args) {
 
 		int number = -2;// 変数numberを宣言し、数値を代入
-		System.out.println(number+"です。");// 「 です。」と画面に表示
+		System.out.println(number + "です。");// 「 です。」と画面に表示
 
 		if (number >= 0) { // もしnumberが0以上の場合、
 			System.out.println("0以上です");// 「0以上です」と表示
