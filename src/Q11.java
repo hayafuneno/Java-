4f77518dbf37d9aaea2d6223e8eb4b9e609898b3
hayafuneno <hayafune.syota@spi-jp.com>
2024-02-27 19:15:21 +0900

    長さの単位センチメートルをミリメートルに変換するプログラム

--- src/Q11.java
+++ src/Q11.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 //https://paiza.jp/challenges/471/show
 public class Q11 {
-	// 1 センチメートルは 10 ミリメートルに変換される
+	// 1センチメートルを10ミリメートルに変換するプログラム
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);// 文字列の受け取り
-		
+
 		int lineInt = Integer.parseInt(sc.nextLine());
 
 //		int lineInt = sc.nextInt();// 数字であるからString型ではなく、int型, キーボードからの入力を取得する
