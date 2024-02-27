af7e1a1498f2b5632076701448698478a78ad542
hayafuneno <hayafune.syota@spi-jp.com>
2024-02-27 18:50:33 +0900

    N人の選手で総当り対戦を行う場合、何回の対戦が行われるか出力(一部修正）

--- src/Q16.java
+++ src/Q16.java
@@ -6,9 +6,9 @@ public class Q16 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);// 文字列の受け取り
 		
-		int number = Integer.parseInt(sc.nextLine());//数字であるからString型ではなく、int型, キーボードからの入力を取得する
-													//（例）4人いたら、4×（4-1）÷2＝「6回」の対戦が行われる
-													//（例）5人いたら、5×（5-1）÷2＝「10回」の対戦が行われる
+		int number = sc.nextInt();//数字であるからString型ではなく、int型, キーボードからの入力を取得する
+		//（例）4人いたら、4×（4-1）÷2＝「6回」の対戦が行われる
+		//（例）5人いたら、5×（5-1）÷2＝「10回」の対戦が行われる
 		System.out.println(number *(number-1)/2);//自分から見た結果と相手側から見た結果で、【N×(N-1)/2】で求められる
 	}
 }
