6e39c82426b9362dd19149842179981b76b4a4ad
hayafuneno <hayafune.syota@spi-jp.com>
2024-02-28 14:45:28 +0900

    「D079:同じ文字」すべて同じ文字であった場合、警報を出力するプログラム（Q6の修正版）

--- src/Q7.java
+++ src/Q7.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+//「D079:同じ文字」
 public class Q7 {
 
 	public static void main(String[] args) {
@@ -12,18 +13,18 @@ public class Q7 {
 //		}
 		boolean chkEqual = isEqual(wordLsit);
 		if (chkEqual) {
-			//Trueなら「NG」を出力
+			// Trueなら「NG」を出力
 			System.out.println("NG");
 		} else {
-			//Falseなら「OK」を出力
+			// Falseなら「OK」を出力
 			System.out.println("OK");
 		}
 	}
 
-	private static boolean isEqual(String[] words) {//メソッド、ローカル変数
-		for (int i = 1; i < words.length; i++) {//「1」からはじめる理由は、はじめの自分と比べる必要がないから
+	private static boolean isEqual(String[] words) {// メソッド、ローカル変数
+		for (int i = 1; i < words.length; i++) {// 「1」からはじめる理由は、はじめの自分と比べる必要がないから
 			if (!words[0].equals(words[i])) {
-				return false;//終了して返す、returnの後にロジックは書けない、処理が終了
+				return false;// 終了して返す、returnの後にロジックは書けない、処理が終了
 			}
 		}
 		return true;
