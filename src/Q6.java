d592331ae2c65ae88210a1f57827216827ecbe40
hayafuneno <hayafune.syota@spi-jp.com>
2024-02-28 14:40:27 +0900

    「D079:同じ文字」すべて同じ文字であった場合、警報を出力するプログラム

--- src/Q6.java
+++ src/Q6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-//D079:同じ文字
+//「D079:同じ文字」
 //すべて同じ文字であった場合、警報を出力する
 //https://paiza.jp/challenges/207/show
 public class Q6 {
@@ -11,8 +11,8 @@ public class Q6 {
 		String word = sc.nextLine();// キーボードで入力された値を取得する
 		String[] words = word.split("");// 一文字ずつ配列に格納
 
-		//boolean型の変数を宣言
-		boolean same = true; 
+		// boolean型の変数を宣言
+		boolean same = true;
 		for (String i : words) {// 配列の一文字目と各要素を比較
 //			if (words[0] != i) {///Heapを見ていない（スタックかも？）
 //				same = false;
@@ -23,5 +23,5 @@ public class Q6 {
 				break;
 			}
 		}
-	}	
+	}
 }
