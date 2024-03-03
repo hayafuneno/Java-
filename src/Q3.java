b5c46fbfe7667d277751a8c12327025afafe04f2
hayafuneno <hayafune.syota@spi-jp.com>
2024-03-03 23:45:05 +0900

    001_D028:数字の桁数[提出済み]

--- src/Q3.java
+++ src/Q3.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 
+//何桁の数字が表示するプログラム
+//001_D028:数字の桁数[提出済み]
+
 public class Q3 {
 	public static void main(String[] args) {
-
-		System.out.println("数字を入力");
-
-		int a = 0;
 		Scanner sc = new Scanner(System.in);// キーボードからの入力を取得する
-
+		int a = 0;
 		int b = sc.nextInt();
 
 		while (b != 0) {
 			b = b / 10;
 			++a;
-
 		}
 		System.out.println("桁数は" + a);
-
 	}
 }
