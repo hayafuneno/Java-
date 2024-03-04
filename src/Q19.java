977e5f0af1eb0a789d2787b20b3123594257bdad
hayafuneno <hayafune.syota@spi-jp.com>
2024-03-04 17:33:41 +0900

    001_D265:薬の処方【提出済み】

---
+++ src/Q19.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//D265:薬の処方
+//001_D265:薬の処方【提出済み】
+public class Q19 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();// キーボードで入力された値を取得する
+		int b = sc.nextInt();// キーボードで入力された値を取得する
+
+		System.out.println(a *3* b);
+		
+	}
+
+}
